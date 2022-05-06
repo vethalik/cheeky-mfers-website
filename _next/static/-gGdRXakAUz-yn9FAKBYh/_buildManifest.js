ceb58d2c4c9d204cef6b8dcf4d6245756e133b76
vethalik <102335823+vethalik@users.noreply.github.com>
2022-05-06 21:25:51 +0000

    Deploying to gh-pages from @ vethalik/cheeky-mfers-website@79768a338719d17307f7e6e1f64f064f0351c650 🚀

--- _next/static/-gGdRXakAUz-yn9FAKBYh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/771-fd151ac35e788b22.js","static/chunks/pages/index-8790e8fc09e9ad41.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
