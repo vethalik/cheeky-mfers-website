8162529a3d3f0b78692c551193163b5414ebfc6b
vethalik <102335823+vethalik@users.noreply.github.com>
2022-05-20 19:19:39 +0200

    updated availability

--- pages/index.js
+++ pages/index.js
@@ -285,6 +285,11 @@ const HomePage = () => {
                           <MenuItem value={'3'}>3</MenuItem>
                           <MenuItem value={'4'}>4</MenuItem>
                           <MenuItem value={'5'}>5</MenuItem>
+                          <MenuItem value={'6'}>6</MenuItem>
+                          <MenuItem value={'7'}>7</MenuItem>
+                          <MenuItem value={'8'}>8</MenuItem>
+                          <MenuItem value={'9'}>9</MenuItem>
+                          <MenuItem value={'10'}>10</MenuItem>
                         </Select>
                       </FormControl>
                     </div>
